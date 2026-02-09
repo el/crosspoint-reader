f1bfb3756cebcf7c7201f7e25da7037da415219a
Eliz Kilic <elizk@google.com>
2026-02-09 22:50:35 +0000

    header

--- lib/FsHelpers/FsHelpers.h
+++ lib/FsHelpers/FsHelpers.h
@@ -2,7 +2,10 @@
 #include <string>
 
 class FsHelpers {
- public:
-  static std::string normalisePath(const std::string& path);
-  static bool saveFramebufferAsBmp(const char* filename, const uint8_t* framebuffer, int width, int height);
+public:
+  static std::string normalisePath(const std::string &path);
+  static bool saveFramebufferAsBmp(const char *filename,
+                                   const uint8_t *framebuffer, int width,
+                                   int height,
+                                   GfxRenderer::Orientation orientation);
 };
